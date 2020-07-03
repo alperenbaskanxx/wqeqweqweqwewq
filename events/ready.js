dbb2e5709a88a4221c177acca7ee73a4886cb8d3
alperenbaskanxx <67757981+alperenbaskanxx@users.noreply.github.com>
2020-07-03 16:11:37 +0300

    Add files via upload

---
+++ events/ready.js
@@ -0,0 +1,31 @@
+// Bu Altyapı Tamamen TlhaMerte Aittir.
+const chalk = require("chalk");
+const moment = require("moment");
+const Discord = require("discord.js");
+const ayarlar = require("../ayarlar.json");
+
+var prefix = ayarlar.prefix;
+
+module.exports = client => {
+  console.log(
+    `[${moment().format("YYYY-MM-DD HH:mm:ss")}] BOT: Aktif, Komutlar yüklendi!`
+  );
+  console.log(
+    `[${moment().format("YYYY-MM-DD HH:mm:ss")}] BOT: ${
+      client.user.username
+    } ismi ile giriş yapıldı!`
+  );
+  client.user.setStatus("online");
+  var oyun = [
+    "Developer : Journey",
+    "Kanal : TlhaMert",
+    "Kanala Abone Olup Bildirimleri Açmayı Unutma 🤠"
+
+  ];
+
+  setInterval(function() {
+    var random = Math.floor(Math.random() * (oyun.length - 0 + 1) + 0);
+
+    client.user.setActivity(oyun[random], "");
+  }, 2 * 2500);
+};
